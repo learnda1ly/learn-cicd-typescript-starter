3d034be158765e7e15068b178f3ce39c472badd6
Stephen Quinlan <55671930+learnda1ly@users.noreply.github.com>
2026-06-24 20:32:01 -0500

    Revert "added code formatting workflow job"

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -3,13 +3,13 @@ import { getAPIKey } from "../api/auth.js";
 
 describe("authentication", () => {
   test("when authentication header is missing", () => {
-    const header = {};
+    const header = {}
     expect(getAPIKey(header)).toBeNull();
   });
   test("when authentication header is imporoperly formed", () => {
     const header = {
-      authentication: "apikey 1234",
-    };
+      authentication: "apikey 1234"
+    }
     expect(getAPIKey(header)).toBeNull();
   });
 });
