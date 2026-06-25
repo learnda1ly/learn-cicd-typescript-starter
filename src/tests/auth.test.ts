5988237541ef8172eb6fd8de65067dc923fb12b3
learnda1ly <stephen.a.quinlan@gmail.com>
2026-06-24 19:14:02 -0500

    started test add.  committing failing test to validate ci configuration

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.js";
+
+describe("authentication", () => {
+  test("when authentication header is missing", () => {
+    const header = {}
+    expect(getAPIKey(header)).toBeNull();
+  });
+  test("when authentication header is imporoperly formed", () => {
+    const header = {
+      authentication: "apikey 1234"
+    }
+    //expect(getAPIKey(header)).toBeNull();
+    expect(getAPIKey(header)).toBe("1234");
+  });
+});
